62a180c4d2b729862ba2989e701071be6561c4bb
Sangin <sangin.kang@navercorp.com>
2019-08-16 05:53:02 +0900

    source update

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,14 +1,25 @@
 import 'package:flutter/material.dart';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  int counter = 0;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        body: Center(child: Text('$counter')),
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add),
           onPressed: () {
-            print('Hi there');
+            setState(() {
+              counter += 1;
+              print(counter);
+            });
           },
         ),
         appBar: AppBar(
